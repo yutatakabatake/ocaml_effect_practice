6dcd328a32ff0c9e7a8d73ae053fa6a9ac501829
yutatakabatake <yutatakabatake@gmail.com>
2024-04-24 17:24:42 -0700

    loop

--- src/life_game.ml
+++ src/life_game.ml
@@ -2,7 +2,6 @@ let xsize = 5
 let ysize = 5
 
 let now_array = Array.make_matrix (ysize+2) (xsize+2) 0
-let next_array = Array.make_matrix (ysize+2) (xsize+2) 0
 
 let int_to_mark n = 
   match n with
@@ -52,32 +51,28 @@ let count array (xsize:int) (ysize:int) =
   in 
   f 0 0 *)
 
-let show_array array xsize ysize = 
+(* let show_array array xsize ysize = 
   for i = 1 to ysize do 
     for j = 1 to xsize do 
       print_int array.(i).(j);
     done;
     print_newline ()
-  done
+  done *)
 
-let rule now_array next_array x y = 
-  match now_array.(y).(x) with
-  | 2 -> next_array.(y).(x) <- 1
-  | 3 -> next_array.(y).(x) <- 1
-  | _ -> next_array.(y).(x) <- 0
+let rule now_array count_array next_array x y = 
+  if now_array.(y).(x) == 0 && count_array.(y).(x) == 3 then next_array.(y).(x) <- 1
+  else if now_array.(y).(x) == 1 && count_array.(y).(x) == 2 then next_array.(y).(x) <- 1
+  else if now_array.(y).(x) == 1 && count_array.(y).(x) == 3 then next_array.(y).(x) <- 1
+  else if now_array.(y).(x) == 1 && count_array.(y).(x) <= 1 then next_array.(y).(x) <- 0
+  else if now_array.(y).(x) == 1 && count_array.(y).(x) >= 4 then next_array.(y).(x) <- 0
 
-let make_next_array now_array next_array xsize ysize = 
+let make_next_array now_array count_array next_array xsize ysize = 
   for i = 0 to xsize -1 do 
     for j = 0 to ysize -1 do 
-      rule now_array next_array j i;
+      rule now_array count_array next_array j i;
     done;
   done;
   next_array
-
-
-let _ = display now_array xsize ysize
-
-let _ = show_array now_array xsize ysize
 
 let _ = change now_array 1 1 
 let _ = change now_array 2 2
@@ -85,19 +80,29 @@ let _ = change now_array 4 3
 let _ = change now_array 1 4
 let _ = change now_array 2 4
 
-let _ = display now_array xsize ysize
+let gen = 5
+let loop = 
+  let rec f now_array n = 
+    if n < gen then g now_array n 
+    else display now_array xsize ysize
+  and g now_array n = 
+      (* print_string ((string_of_int n) ^ "世代" ^ "\n");
+      print_string "display now_array\n"; *)
+      display now_array xsize ysize;
+      (* print_string "show now_array\n";  *)
+      (* show_array now_array xsize ysize; *)
+      let count = count now_array xsize ysize in 
+      let next_array = make_next_array now_array count (Array.make_matrix (ysize+2) (xsize+2) 0) xsize ysize in 
+      (* print_newline ();
+      print_string "show count\n";
+      show_array count xsize ysize;
+      print_newline ();
+      print_string "show next_array\n";
+      show_array next_array xsize ysize;
+      print_newline ();
+      display next_array xsize ysize; *)
+      f next_array (n+1)
+  in f now_array 0
 
-let _ = show_array now_array xsize ysize
-let _ = print_newline ()
 
-let count = count now_array xsize ysize
-
-let _ = show_array count xsize ysize
-let _ = print_newline ()
-
-let array2 = make_next_array count next_array xsize ysize
-
-let _ = show_array array2 xsize ysize
-let _ = print_newline ()
-
-let _ = display array2 xsize ysize
+let _ = loop 
