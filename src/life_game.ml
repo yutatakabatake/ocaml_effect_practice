f60bb644e095b8a4b4dcafffe6a54e9eee48677f
yutatakabatake <yutatakabatake@gmail.com>
2024-05-08 18:59:12 +0900

    ハンドラで配列を操作する

--- src/life_game.ml
+++ src/life_game.ml
@@ -1,11 +1,11 @@
 open Effect
 open Effect.Deep
 
-type _ Effect.t += Display : int array array -> unit Effect.t
-type _ Effect.t += Count : int array array -> int array array Effect.t
-type _ Effect.t += Make_next_array : (int array array * int array array) -> int array array Effect.t
-type _ Effect.t += Change : (int array array * int * int) -> unit Effect.t
-
+type _ Effect.t += Display : unit -> unit Effect.t
+type _ Effect.t += Count : unit -> int array array Effect.t
+type _ Effect.t += Make_next_array : int array array -> unit Effect.t
+type _ Effect.t += Change : (int * int) -> unit Effect.t
+type _ Effect.t += Step : unit -> unit Effect.t
 
 let xsize = 5
 let ysize = 5
@@ -28,19 +28,20 @@ let rule now_array count_array next_array x y =
   | _ -> ()
 
 let run f = 
+  let now_array = Array.make_matrix (ysize+2) (xsize+2) 0 in 
   let next_array = Array.make_matrix (ysize+2) (xsize+2) 0 in
   match_with f () {
     retc = (fun x -> x);
     exnc = (fun e -> raise e);
     effc = (fun  (type b) (eff: b t)-> 
       (match eff with
-        | Display array -> 
+        | Display () -> 
           (Some (fun (k: (b,_) continuation) ->
             let rec f i j = 
               if i > ysize then ()
               else if j > xsize then (print_newline ();
                                       f (i+1) 1)
-              else ((let str = int_to_mark array.(i).(j) in
+              else ((let str = int_to_mark now_array.(i).(j) in
                       match str with
                       | Some x -> print_string x
                       | None -> ());
@@ -48,7 +49,7 @@ let run f =
             in f 1 1;
             continue k ()
           ))
-        | Count array ->
+        | Count () ->
           (Some (fun (k: (b,_) continuation) ->
             let count_array = Array.make_matrix (ysize+2) (xsize+2) 0 in
             let rec f i j k pre = 
@@ -61,11 +62,11 @@ let run f =
                                       f i (j+1) (-1) 0
                                       )
                         else if k == 0 then(
-                                            let count = array.(i).(j-1) + array.(i).(j+1) in 
+                                            let count = now_array.(i).(j-1) + now_array.(i).(j+1) in 
                                             f i j (k+1) (count+pre)
                                             )
                         else (
-                              let count = array.(i+k).(j-1) + array.(i+k).(j) + array.(i+k).(j+1) in 
+                              let count = now_array.(i+k).(j-1) + now_array.(i+k).(j) + now_array.(i+k).(j+1) in 
                               f i j (k+1) (count+pre)
                               )
                         )
@@ -73,7 +74,7 @@ let run f =
             in f 1 1 (-1) 0;
             continue k count_array
           ))
-        | Make_next_array (now_array,count_array) ->
+        | Make_next_array (count_array) ->
           (Some (fun (k: (b,_) continuation) ->
             let rec f i j = 
               if i > ysize then ()
@@ -81,42 +82,46 @@ let run f =
               else (rule now_array count_array next_array j i;
                     f i (j+1))
             in f 1 1;
-          continue k next_array
+            continue k ()
           ))
-        | Change (array,x,y) ->
+        | Change (x,y) ->
           (Some (fun (k: (b,_) continuation) ->
-            (match array.(y).(x) with
-            | 0 -> array.(y).(x) <- 1
-            | 1 -> array.(y).(x) <- 0
+            (match now_array.(y).(x) with
+            | 0 -> now_array.(y).(x) <- 1
+            | 1 -> now_array.(y).(x) <- 0
             | _ -> ()
             );
+            continue k ()
+          ))
+        | Step () ->
+          (Some (fun (k: (b,_) continuation) ->
+            let rec f i j = 
+              if i > ysize then ()
+              else if j > xsize then (f (i+1) 1)
+              else (now_array.(i).(j) <- next_array.(i).(j) ;
+                    f i (j+1))
+            in f 1 1;
             continue k ()
           ))
         | _ -> None)
     )
   }
 
-let now_array = Array.make_matrix (ysize+2) (xsize+2) 0
+let main () =
+  let _ = perform (Change (1,1)) in 
+  let _ = perform (Change (2,2)) in 
+  let _ = perform (Change (4,3)) in 
+  let _ = perform (Change (1,4)) in 
+  let _ = perform (Change (2,4)) in 
+  let rec loop n = 
+    if n < gen then (let _ = perform (Display ()) in 
+                     let count = perform (Count ()) in 
+                     let _ = perform (Make_next_array count) in 
+                     let _ = perform (Step ()) in 
+                     print_newline ();
+                     (loop (n+1))
+                    )
+    else perform (Display ()) 
+  in loop 0
 
-let change_array_element () = 
-  let _ = perform (Change (now_array,1,1)) in 
-  let _ = perform (Change (now_array,2,2)) in 
-  let _ = perform (Change (now_array,4,3)) in 
-  let _ = perform (Change (now_array,1,4)) in 
-  let _ = perform (Change (now_array,2,4)) in 
-  ()
-
-let loop () = 
-  let rec f now_array n = 
-    if n < gen then g now_array n 
-    else perform (Display now_array)
-  and g now_array n = 
-      let _ = perform (Display now_array) in 
-      let count = perform (Count now_array) in 
-      let next_array = perform (Make_next_array (now_array,count)) in 
-      print_newline ();
-      f next_array (n+1)
-  in f now_array 0
-
-let _ = run change_array_element
-let _ = run loop 
+let _ = run main 
