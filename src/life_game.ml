4d6a7acb7a213c64e32b546da38a160162d119a6
yutatakabatake <yutatakabatake@gmail.com>
2024-05-08 16:18:06 +0900

    DisplayをEffectを使ったものにした

--- src/life_game.ml
+++ src/life_game.ml
@@ -1,7 +1,11 @@
+open Effect
+open Effect.Deep
+
+type _ Effect.t += Display : int array array -> unit Effect.t
+
 let xsize = 5
 let ysize = 5
-
-let now_array = Array.make_matrix (ysize+2) (xsize+2) 0
+let gen = 5
 
 let int_to_mark n = 
   match n with
@@ -9,17 +13,32 @@ let int_to_mark n =
   | 1 -> Some "*"
   | _ -> None 
 
-let display array = 
-  let rec f i j = 
-    if i > ysize then ()
-    else if j > xsize then ( print_newline ();
-                            f (i+1) 1 )
-    else ( (let str = int_to_mark array.(i).(j) in
-            match str with
-            | Some x -> print_string x
-            | None -> ());
-          f i (j+1) )
-  in f 1 1 
+
+let run f = 
+  match_with f () {
+    retc = (fun x -> x);
+    exnc = (fun e -> raise e);
+    effc = (fun  (type b) (eff: b t)-> 
+      (match eff with
+        | Display (array) -> 
+          (Some (fun (k: (b,_) continuation) ->
+            let rec f i j = 
+              if i > ysize then ()
+              else if j > xsize then (print_newline ();
+                                      f (i+1) 1)
+              else ((let str = int_to_mark array.(i).(j) in
+                      match str with
+                      | Some x -> print_string x
+                      | None -> ());
+                    f i (j+1))
+            in f 1 1;
+            continue k ()
+          ))
+        | _ -> None)
+    )
+  }
+
+let now_array = Array.make_matrix (ysize+2) (xsize+2) 0
 
 let change array x y = 
   match array.(y).(x) with
@@ -78,16 +97,16 @@ let _ = change now_array 4 3
 let _ = change now_array 1 4
 let _ = change now_array 2 4
 
-let gen = 5
-let loop = 
+let loop () = 
   let rec f now_array n = 
     if n < gen then g now_array n 
-    else display now_array
+    else perform (Display now_array)
   and g now_array n = 
-      display now_array;
+      let _ = perform (Display now_array) in 
       let count = count now_array in 
       let next_array = make_next_array now_array count (Array.make_matrix (ysize+2) (xsize+2) 0) in 
       f next_array (n+1)
   in f now_array 0
 
-let _ = loop 
+
+let _ = run (loop) 
