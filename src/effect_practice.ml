bd31ad45d3b7abc571780cbda0ac1193316b6232
yutatakabatake <yutatakabatake@gmail.com>
2024-08-09 14:51:10 +0900

    alwaysReadのexample追加

--- src/effect_practice.ml
+++ src/effect_practice.ml
@@ -13,19 +13,39 @@ let handler f =
       (match eff with
         | Read () ->
           (Some (fun (k: (b,_) continuation) -> 
-            let x = "Bob" in 
-            continue k x)) (* 値を持って継続を再開 *)
+            continue k "Bob")) (* 値を持って継続を再開 *)
         | _ -> None)
     )
   }
 
+(* alwaysRead = fun s -> handler {read(_,k) -> k s} *)
+let alwaysRead = 
+  fun (s : string) -> 
+    let handler2 f = 
+      match_with f () {
+        retc = (fun x -> x);
+        exnc = (fun e -> raise e);
+        effc = (fun (type b) (eff: b t) -> 
+          (match eff with
+            | Read () ->
+              (Some (fun (k: (b,_) continuation) -> 
+                continue k s)) 
+            | _ -> None)
+        )
+      } in handler2
+
+
 (* 実行されるプログラム *)
 let printFullName () = 
-  (* print_endline ("What is your forename?"); *)
+  print_endline ("What is your forename?");
   let forename = perform (Read ()) in 
-    (* print_endline ("What is your surname?"); *)
+    print_endline ("What is your surname?");
     let surname = perform (Read ()) in 
       print_endline (forename ^ surname)
 
 
+let _ = print_endline ("with handler handle printFullName")
 let _ = handler printFullName 
+
+let _ = print_endline ("with (alwaysRead Bob) handle printFullName")
+let _ = alwaysRead "Bob" printFullName
